419c028f9365bb3ac756806513bd7eff84d5c9d1
Manas Singhal <singhalmanas20@gmail.com>
2025-04-20 15:14:04 +0530

    add:basic-app-setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View, StyleSheet, Platform, Image } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{justifyContent: "center", alignItems: "center"}}>
+      <Text>Hello World 👋 🌍!</Text>
+    </View>
+  );
+}
