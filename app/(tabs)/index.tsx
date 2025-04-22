fa120ed6673deb3069a95656c50604afa5e84d61
Manas Singhal <singhalmanas20@gmail.com>
2025-04-22 10:52:51 +0530

    testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet, Platform, Image } from "react-native";
+import { Text, View} from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={{justifyContent: "center", alignItems: "center"}}>
-      <Text>Hello World 👋 🌍!</Text>
+      <Text>Hello Manas 👋 🌍!</Text>
     </View>
   );
 }
